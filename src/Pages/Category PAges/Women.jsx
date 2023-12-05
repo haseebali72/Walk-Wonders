3d80f9979d137e1cc260da355f65a7f0342bfaf8
Haseeb Ali <smhaseebali8@gmail.com>
2023-12-06 03:59:46 +0500

    Changed Women.jsx code from Men.jsx dummy code

--- src/Pages/Category PAges/Women.jsx
+++ src/Pages/Category PAges/Women.jsx
@@ -1,35 +1,39 @@
-import { products } from '../../data/Products'
+import { products } from '../../data/Products';
 import { useDispatch } from 'react-redux';
 import { addItem } from '../../redux/slices/cartSlice';
 import { addCount } from '../../redux/slices/addItemCounterSlice';
+import { NavLink } from 'react-router-dom';
+import ProductPage from '../General Pages/ProductPage';
 
-const Women = () => {
+const Women = () => { 
   const dispatch = useDispatch();
+
   const addItemHandler = (p) => {
     dispatch(addItem(p));
-    dispatch(addCount())
+    dispatch(addCount({itemId : p.id, quantity:0}))
   };
-    const womensProduct = products.filter((p) => p.gender === 'WOMEN');
-  
-    return (
-      <div className="flex flex-wrap">
-        {womensProduct.map((p) => (
-          <div key={p.id} className='w-64 h-84 m-2 hover:border border-black'>
-            <div className='w-auto h-auto flex'>
-              <img className='' src={p.imageURL} alt={p.name} />
-            </div>
-            <div className='w-auto h-auto block '>
-              <p>{p.name}-{p.gender}</p>
-              <p>{p.price}</p>
-              <button className='bg-red-700 border border-red-950 font text-white mb-3 ml-7 ' onClick={()=> addItemHandler(p)}>
 
-              Add to Cart
-            </button>
-            </div>
+  const womenProduct = products.filter((p) => p.gender === 'WOMEN');
+
+  return (
+    <div className="flex flex-wrap">
+      {womenProduct.map((p) => (
+        <NavLink to='/product'>
+        <div key={p.id} className='w-64 h-84 m-2  hover:shadow-lg transition-transform transform hover:translate-y-0.5'>
+          <div className='w-auto h-auto flex'>
+            <img className='' src={p.imageURL} alt={p.name} />
           </div>
-        ))}
-      </div>
-    );
-  }
+          <div className='w-auto h-auto block '>
+            <p>{p.name}-{p.gender}</p>
+            <p>{p.price}</p>
+            <button className='bg-red-700 border border-red-950 font text-white mb-3 ml-7 ' onClick={() => addItemHandler(p)}>Add to Cart
+            </button>
+          </div>
+        </div>
+        </NavLink>
+      ))}
+    </div>
+  );
+};
 
-export default Women
+export default Women;
