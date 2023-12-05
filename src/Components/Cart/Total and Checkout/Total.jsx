76785ee6493717a74830e009ab52fe50651783e1
Haseeb Ali <smhaseebali8@gmail.com>
2023-12-06 04:55:46 +0500

    Given hover effect to Checkout Button

--- src/Components/Cart/Total and Checkout/Total.jsx
+++ src/Components/Cart/Total and Checkout/Total.jsx
@@ -9,7 +9,7 @@ const Total = () => {
     <>
       <div className='bg-white p-4 flex flex-col items-center'>
         <h1 className='mb-2'>Subtotal({items.length} Items): <b>${totalPrice}</b></h1>
-        <button className='bg-yellow-400 px-4 py-2 rounded-md'><b>Proceed to checkout</b></button>
+        <button className='bg-yellow-400 px-4 py-2 rounded-md hover:bg-yellow-500'><b>Proceed to checkout</b></button>
       </div>
     </>
   );
